6e3ccb3eb7b9a7d71690b7b23d7cf51c1d0136c8
Pol <pol.sanchez@lifullconnect.com>
2020-02-03 17:52:22 +0100

    feat: Accomplish converting the exceptions by changing HashMap

--- RomansConverter/src/main/java/RomansConverter.java
+++ RomansConverter/src/main/java/RomansConverter.java
@@ -8,37 +8,33 @@ public class RomansConverter {
 
     public String romanConversion(int number) {
         romanValues.put("M", 1000);
+        romanValues.put("CM", 900);
         romanValues.put("D", 500);
+        romanValues.put("CD", 400);
         romanValues.put("C", 100);
+        romanValues.put("XC", 90);
         romanValues.put("L", 50);
+        romanValues.put("XL", 40);
         romanValues.put("X", 10);
+        romanValues.put("IX", 9);
         romanValues.put("V", 5);
+        romanValues.put("IV", 4);
         romanValues.put("I", 1);
 
         String romanNumber = "";
         int restNumber = number;
-        int countRepeatNum = 0;
-        Map.Entry <String, Integer> lastEntry;
-
 
         for (Map.Entry<String, Integer> entryRomanValues : romanValues.entrySet()) {
             String keyRomanChar = entryRomanValues.getKey();
             Integer valueRomanNumber = entryRomanValues.getValue();
 
-
             while(restNumber >= valueRomanNumber){
-                if(countRepeatNum > 3){
+
+                restNumber -= valueRomanNumber;
+                romanNumber += keyRomanChar;
 
 
-                }else{
-                    restNumber -= valueRomanNumber;
-                    romanNumber += keyRomanChar;
-                }
-                countRepeatNum++;
             }
-
-            countRepeatNum = 0;
-            lastEntry = entryRomanValues;
         }
 
        return romanNumber;
--- RomansConverter/src/test/java/RomansConverterShould.java
+++ RomansConverter/src/test/java/RomansConverterShould.java
@@ -11,9 +11,12 @@ public class RomansConverterShould {
             "2,II",
             "3,III",
             "5,V",
-            "10,X"//,
-            //"4,IV"
-            //,"11,XI"
+            "10,X",
+            "44,XLIV",
+            "4,IV",
+            "11,XI",
+            "666,DCLXVI",
+            "3999,MMMCMXCIX"
     })
     public void convert_arabic_numbers_to_roman_numbers(int arabic, String roman){
         RomansConverter romansConverter = new RomansConverter();
