e57408d5dd77996a684d5349c5e937c01412ab72
Natalia Terrones <nterrones@LC02RL0QYFVH4.office>
2020-02-04 12:16:32 +0100

    Reorder hasmap to access to biggers numbers

--- RomansConverterTPP/src/main/java/RomansConverterTPP.java
+++ RomansConverterTPP/src/main/java/RomansConverterTPP.java
@@ -1,20 +1,28 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 public class RomansConverterTPP {
     HashMap <Integer, String > romanNumbers = new LinkedHashMap<>();
-
+    Iterator iteratorRomanNumbers;
     public RomansConverterTPP() {
-        romanNumbers.put(1, "I");
-        romanNumbers.put(4, "IV");
+        romanNumbers.put(40, "XL");
+        romanNumbers.put(10, "X");
+        romanNumbers.put(9, "IX");
         romanNumbers.put(5, "V");
+        romanNumbers.put(4, "IV");
+        romanNumbers.put(1, "I");
+        iteratorRomanNumbers = romanNumbers.entrySet().iterator();
+
     }
 
     public String romanConversion(int number) {
         if (romanNumbers.containsKey(number)){
             return romanNumbers.get(number);
+        }
+        if (number > 40){
+            return "XL" + romanConversion(number - 40);
+        }
+        if (number > 10){
+            return "X" + romanConversion(number - 10);
         }
         if (number > 5){
             return "V" + romanConversion(number - 5);
--- RomansConverterTPP/src/test/java/RomansConverterTPPShould.java
+++ RomansConverterTPP/src/test/java/RomansConverterTPPShould.java
@@ -6,13 +6,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RomansConverterTPPShould {
     @ParameterizedTest
     @CsvSource({
-            "1,I",
-            "2,II",
-            "3,III",
+            "1, I",
+            "2, II",
+            "3, III",
             "4, IV",
             "5, V",
             "6, VI",
-            "7, VII"
+            "7, VII",
+            "9, IX",
+            "10, X",
+            "11, XI",
+            "40, XL",
+            "41, XLI"
     })
     public void convert_arabic_numbers_to_roman_numbers(int arabic, String roman){
         RomansConverterTPP romansConverter = new RomansConverterTPP();
