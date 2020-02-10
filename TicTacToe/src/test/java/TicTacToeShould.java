0b1b3b31677ac4cb823147a47aaac1d53de42c5d
Laura González <lgonzalez@LC02T517ZFVH4.office>
2020-02-10 09:41:04 +0100

    refactor: remove empty spaces

--- TicTacToe/src/test/java/TicTacToeShould.java
+++ TicTacToe/src/test/java/TicTacToeShould.java
@@ -7,7 +7,6 @@ public class TicTacToeShould {
 
     @Test
     public void return_o_for_the_second_move(){
-
         Tictactoe tictactoe = new Tictactoe();
         tictactoe.makeMove();
         assertEquals("o", tictactoe.makeMove());
@@ -19,7 +18,6 @@ public class TicTacToeShould {
         tictactoe.makeMove();
         tictactoe.makeMove();
         assertEquals("x", tictactoe.makeMove());
-
     }
 
 
