42bc10f00e3bb2805dc8500b5b3ead4a0260ed89
Laura González <lgonzalez@LC02T517ZFVH4.office>
2020-02-10 09:39:18 +0100

    Feat: Return x for the third move

--- TicTacToe/src/main/java/Tictactoe.java
+++ TicTacToe/src/main/java/Tictactoe.java
@@ -1,10 +1,13 @@
 public class Tictactoe {
 
-    String lastMove;
-
+    String lastMove = "o";
 
     public String makeMove() {
-
+        if (lastMove.equals("o")){
+            lastMove = "x";
+            return "x";
+        }
+        lastMove = "o";
         return "o";
     }
 }
--- TicTacToe/src/test/java/TicTacToeShould.java
+++ TicTacToe/src/test/java/TicTacToeShould.java
@@ -13,6 +13,15 @@ public class TicTacToeShould {
         assertEquals("o", tictactoe.makeMove());
     }
 
+    @Test
+    public void return_x_for_the_third_move(){
+        Tictactoe tictactoe = new Tictactoe();
+        tictactoe.makeMove();
+        tictactoe.makeMove();
+        assertEquals("x", tictactoe.makeMove());
+
+    }
+
 
 
 }
