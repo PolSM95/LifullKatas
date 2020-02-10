36db40004e2e495194f330942f430bf06ed061eb
MAbella <miriam.abella@lifullconnect.com>
2020-02-10 16:44:11 +0100

    Initial commit, new project

---
+++ TicTacToeCalisthenics/src/test/java/TicTacToeCalisthenicsShould.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TicTacToeCalisthenicsShould {
+
+    @Test
+    public void test(){
+        
+    }
+
+
+}
