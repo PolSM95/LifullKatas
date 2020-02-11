515c012dd7f270b98eaad035ef5ec804120413c7
Pol <pol.sanchez@lifullconnect.com>
2020-02-11 15:17:58 +0100

    Default project final

---
+++ defaultProject/src/test/java/defaultShould.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class defaultShould {
+    @Test
+    public void Test(){
+        assertEquals("0","0");
+    }
+}
