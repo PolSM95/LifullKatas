b7555d025c53e0125f45c5e5972813f160193bfd
mbarriolifull <marc.barrio@lifullconnect.com>
2020-02-13 15:36:38 +0100

    refactor: removed else in checkSCoreWhenIsMoreThanFortyPoints

--- Refactoring-tennis/src/main/java/TennisGame1.java
+++ Refactoring-tennis/src/main/java/TennisGame1.java
@@ -37,13 +37,11 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String checkScoreWhenIsMoreThanFortyPoints() {
-        String score;
         int minusResult = scorePlayer1 - scorePlayer2;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
-        return score;
+        if (minusResult==1) return "Advantage player1";
+        if (minusResult ==-1) return "Advantage player2";
+        if (minusResult>=2) return "Win for player1";
+        return "Win for player2";
     }
 
     private String checkDrawType() {
