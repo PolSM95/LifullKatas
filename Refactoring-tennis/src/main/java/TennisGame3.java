8096bb48e05d5666b81418b76bcefa0fa1d862cc
Natalia Terrones <nterrones@LC02RL0QYFVH4.office>
2020-02-14 10:26:58 +0100

    Refactor: Create enumList to follow Calisthenics rules

--- Refactoring-tennis/src/main/java/TennisGame3.java
+++ Refactoring-tennis/src/main/java/TennisGame3.java
@@ -32,16 +32,17 @@ public class TennisGame3 implements TennisGame {
 
 
     public String getScore() {
-        String s;
+        String resultMessage;
         if (player1Score < 4 && player2Score < 4 && !(player1Score + player2Score == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[player1Score];
-            return (player1Score == player2Score) ? s + "-All" : s + "-" + p[player2Score];
+            Messages[] enumList = new Messages[]{Messages.LOVE,Messages.FIFTEEN,Messages.THIRTY,Messages.FORTY};
+            String[] scoreList = new String[]{enumList[0].message, enumList[1].message, enumList[2].message,enumList[3].message};
+            resultMessage = scoreList[player1Score];
+            return (player1Score == player2Score) ? resultMessage + "-All" : resultMessage + "-" + scoreList[player2Score];
         } else {
             if (player1Score == player2Score)
                 return "Deuce";
-            s = player1Score > player2Score ? player1ScoreMessage : player2ScoreMessage;
-            return ((player1Score - player2Score)*(player1Score - player2Score) == 1) ? "Advantage " + s : "Win for " + s;
+            resultMessage = player1Score > player2Score ? player1ScoreMessage : player2ScoreMessage;
+            return ((player1Score - player2Score)*(player1Score - player2Score) == 1) ? "Advantage " + resultMessage : "Win for " + resultMessage;
         }
     }
     
