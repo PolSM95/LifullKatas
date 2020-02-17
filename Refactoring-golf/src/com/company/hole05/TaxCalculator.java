d729fc5faf27831b319f35b97687309b04d1e50d
Laura Gonzalez Fernandez <lauragonzafernan@gmail.com>
2020-02-17 11:11:32 +0100

    Refactor

--- Refactoring-golf/src/com/company/hole05/TaxCalculator.java
+++ Refactoring-golf/src/com/company/hole05/TaxCalculator.java
@@ -2,9 +2,11 @@ package com.company.hole05;
 
 public class TaxCalculator {
     public double taxFor(final double grossSalary) {
-        final double lowerTaxBracketGross = Math.max(Math.min(grossSalary, 20000.0) - 5000, 0.0);
-        final double middleTaxBracketGross = Math.max(Math.min(grossSalary, 40000) - 20000, 0.0);
-        final double upperTaxBracketGross = Math.max(grossSalary - 40000, 0.0);
+        final int middleTaxBracketStart = 20000;
+        final double lowerTaxBracketGross = Math.max(Math.min(grossSalary, middleTaxBracketStart) - 5000, 0.0);
+        final int upperTaxBracketStart = 40000;
+        final double middleTaxBracketGross = Math.max(Math.min(grossSalary, upperTaxBracketStart) - middleTaxBracketStart, 0.0);
+        final double upperTaxBracketGross = Math.max(grossSalary - upperTaxBracketStart, 0.0);
         return lowerTaxBracketGross * 0.1 + middleTaxBracketGross * 0.2 + upperTaxBracketGross * 0.4;
     }
 }
