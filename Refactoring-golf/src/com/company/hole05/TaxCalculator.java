4924f0c040f06030a61d99d883bb72c26fb32c3d
MAbella <miriam.abella@lifullconnect.com>
2020-02-17 11:38:50 +0100

    Refactor: extract all vars in hole05

--- Refactoring-golf/src/com/company/hole05/TaxCalculator.java
+++ Refactoring-golf/src/com/company/hole05/TaxCalculator.java
@@ -1,12 +1,20 @@
 package com.company.hole05;
 
 public class TaxCalculator {
+
     public double taxFor(final double grossSalary) {
-        final int middleTaxBracketStart = 20000;
-        final double lowerTaxBracketGross = Math.max(Math.min(grossSalary, middleTaxBracketStart) - 5000, 0.0);
-        final int upperTaxBracketStart = 40000;
-        final double middleTaxBracketGross = Math.max(Math.min(grossSalary, upperTaxBracketStart) - middleTaxBracketStart, 0.0);
+        final double upperTaxBracketStart = 40000;
+        final double upperTaxBracketRate = 0.4;
         final double upperTaxBracketGross = Math.max(grossSalary - upperTaxBracketStart, 0.0);
-        return lowerTaxBracketGross * 0.1 + middleTaxBracketGross * 0.2 + upperTaxBracketGross * 0.4;
+        
+        final double middleTaxBracketStart = 20000;
+        final double middleTaxBracketRate = 0.2;
+        final double middleTaxBracketGross = Math.max(Math.min(grossSalary, upperTaxBracketStart) - middleTaxBracketStart, 0.0);
+        
+        final double lowerTaxBracketStart = 5000;
+        final double lowerTaxBracketRate = 0.1;
+        final double lowerTaxBracketGross = Math.max(Math.min(grossSalary, middleTaxBracketStart) - lowerTaxBracketStart, 0.0);
+        
+        return lowerTaxBracketGross * lowerTaxBracketRate + middleTaxBracketGross * middleTaxBracketRate + upperTaxBracketGross * upperTaxBracketRate;
     }
 }
