e30fbb939f7f01966ae6eeeb9440d5f86aabef02
jpena77 <jordi.pena@lifullconnect.com>
2020-02-17 10:19:46 +0100

    refactor: adding taxcalculator as variable and in the constructor

--- Refactoring-golf/src/com/company/hole03/Payslip.java
+++ Refactoring-golf/src/com/company/hole03/Payslip.java
@@ -2,13 +2,15 @@ package com.company.hole03;
 
 public class Payslip {
     private final double grossSalary;
+    private final TaxCalculator taxCalculator;
 
-    public Payslip(final double grossSalary) {
+    public Payslip(final double grossSalary, final TaxCalculator taxCalculator) {
         this.grossSalary = grossSalary;
+        this.taxCalculator = taxCalculator;
     }
 
     public double getNet() {
-        return grossSalary - new TaxCalculator().taxFor(grossSalary);
+        return grossSalary - taxCalculator.taxFor(grossSalary);
     }
 
 }
