f0d503e1d702f0e00d1a4c20aa02b93b3e3b9c2b
jpena77 <jordi.pena@lifullconnect.com>
2020-02-17 14:54:17 +0100

    test: checking that quality of sulfuras never change and the quality of and unknown and unexpired item

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,6 +14,23 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    public void checking_that_sulfuras_quality_never_changes(){
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50,app.items[0].quality);
+    }
+
+    @Test
+    public void checking_quality_for_an_unknown_and_unexpired_item(){
+        Item[] items = new Item[] { new Item("Mango's elixir", 1, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(49,app.items[0].quality);
+    }
+
+
 
 
 }
