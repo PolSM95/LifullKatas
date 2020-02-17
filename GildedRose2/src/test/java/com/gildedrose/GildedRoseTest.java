b7e2a8af089ed8de6c0547386619d432f98028f8
Pol <pol.sanchez@lifullconnect.com>
2020-02-17 17:19:04 +0100

    test: change approval test into automatic PIT Test

--- GildedRose2/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose2/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,15 +1,12 @@
 package com.gildedrose;
 
-import org.approvaltests.Approvals;
 import org.approvaltests.combinations.CombinationApprovals;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class GildedRoseTest {
+public class GildedRoseTest {
 
     @Test
-    void foo() {
+    public void foo() {
         CombinationApprovals.verifyAllCombinations(this::doUpdateQuality,
                 new String[]{
                         "foo",
@@ -17,7 +14,7 @@ class GildedRoseTest {
                         "Backstage passes to a TAFKAL80ETC concert",
                         "Sulfuras, Hand of Ragnaros"
                 },
-                new Integer[]{-1,0,1,5,10,12},new Integer[]{0,1,30,49,50,51,60});
+                new Integer[]{-1,0,1,5,6,10,11,12},new Integer[]{0,1,30,49,50,51,60});
     }
 
     private String doUpdateQuality(String name, int sellIn, int quality) {
