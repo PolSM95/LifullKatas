5969ed60148381809f444191fd3c77cd55af00d6
MAbella <miriam.abella@lifullconnect.com>
2020-02-19 10:28:17 +0100

    return list trip when no friends

--- trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -4,6 +4,9 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
@@ -11,18 +14,32 @@ public class TripServiceTest {
 
     @Test
     public void validate_not_logged_in_user (){
-        TripService testeableTripService = new TesteableTripService();
+        TripService testeableTripService = new TesteableTripService(false);
         assertThrows(UserNotLoggedInException.class, ()->{testeableTripService.getTripsByUser(new User());});
     }
 
+    @Test
+    public void does_not_return_any_trips_when_users_are_not_friends(){
+        TripService testeableTripService = new TesteableTripService(true);
+        assertEquals(new ArrayList<Trip>(), testeableTripService.getTripsByUser(new User()));
+    }
+
+
+
     private class TesteableTripService extends TripService {
+        private final boolean userLogged;
+
+        public TesteableTripService(boolean userLogged){
+            this.userLogged = userLogged;
+        }
+
         @Override
         protected User getLoggedUser() {
+            if (userLogged){
+                return new User();
+            }
             return null;
         }
     }
 
-
-    //does_not_return_any_trips_when_users_are_not_friends
-    //return_trips_when_users_are_friends
 }
