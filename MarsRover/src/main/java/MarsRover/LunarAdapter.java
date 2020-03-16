615480d1f3a4122f4d2f5a8cee0059cc61559cc6
mbarriolifull <marc.barrio@lifullconnect.com>
2020-03-16 15:21:23 +0100

    Feat: implemented move forward/backwards in LunarAdapter

--- MarsRover/src/main/java/MarsRover/LunarAdapter.java
+++ MarsRover/src/main/java/MarsRover/LunarAdapter.java
@@ -1,44 +1,66 @@
 package MarsRover;
 
-import MarsRover.Cardinal.Cardinal;
-import MarsRover.Cardinal.North;
+import MarsRover.Cardinal.*;
 import MarsRover.MarsRoverPackage.AbstractMarsRover;
 
 public class LunarAdapter extends AbstractMarsRover {
 
+    private Cardinal auxiliarOrientation;
+    private LunarRover lunarRover;
 
     public LunarAdapter(Coordinates coordinates, Cardinal orientation) {
         super(coordinates, orientation);
+        auxiliarOrientation = orientation;
+        lunarRover = new LunarRover(coordinates.xCoordinate,coordinates.yCoordinate);
 
     }
 
 
-    public void  rotateAdapter(Coordinates coordinates,Cardinal orientation){
-
+    public void  rotateAdapter(){
 
     }
 
 
     @Override
     public void moveForward() {
-        //if(orientation instanceof North){}
-        //up N
+        if(auxiliarOrientation instanceof North){
+            lunarRover.moveUp();
+        }
+        if (auxiliarOrientation instanceof South){
+            lunarRover.moveDown();
+        }
+        if (auxiliarOrientation instanceof West){
+            lunarRover.moveLeft();
+        }
+        if (auxiliarOrientation instanceof East){
+            lunarRover.moveRight();
+        }
 
     }
 
     @Override
     public void moveBackwards() {
-        //down N
-
+        if(auxiliarOrientation instanceof North){
+            lunarRover.moveDown();
+        }
+        if (auxiliarOrientation instanceof South){
+            lunarRover.moveUp();
+        }
+        if (auxiliarOrientation instanceof West){
+            lunarRover.moveRight();
+        }
+        if (auxiliarOrientation instanceof East){
+            lunarRover.moveLeft();
+        }
     }
 
     @Override
     public void rotateLeft() {
-        super.rotateLeft();
+        auxiliarOrientation.rotateLeft();
     }
 
     @Override
     public void rotateRight() {
-        super.rotateRight();
+        auxiliarOrientation.rotateRight();
     }
 }
