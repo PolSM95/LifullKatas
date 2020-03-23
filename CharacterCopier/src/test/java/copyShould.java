a1e4b346dc20acdf401a807f51b1b31e9804ff22
jpena77 <jordi.pena@lifullconnect.com>
2020-03-23 12:00:31 +0100

    feat: implementing test when endOfLine is at the beginning, in the middel and at the end of the string

--- CharacterCopier/src/test/java/copyShould.java
+++ CharacterCopier/src/test/java/copyShould.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class copyShould {
 
     @Test
-    public void check_If_Read_Character_a(){
+    public void check_the_length_of_a_string(){
 
      ReaderTest readerTest = new ReaderTest("a\n");
      WriterTest writerTest = new WriterTest();
@@ -18,7 +18,31 @@ public class copyShould {
      assertEquals(1, writerTest.writeResultLength());
     }
 
+    @Test
+    public void returning_a_string_when_endOfLine_is_at_the_end(){
+        ReaderTest readerTest = new ReaderTest("abcd\n");
+        WriterTest writerTest = new WriterTest();
+        Copier copier = new Copier(readerTest,writerTest);
+        copier.copy();
+        assertEquals("abcd", writerTest.getWriteResult());
+    }
+    @Test
+    public void returning_empty_string_when_endOfLine_is_at_the_beginning(){
+        ReaderTest readerTest = new ReaderTest("\newq");
+        WriterTest writerTest = new WriterTest();
+        Copier copier = new Copier(readerTest,writerTest);
+        copier.copy();
+        assertEquals("", writerTest.getWriteResult());
+    }
 
+    @Test
+    public void returning_a_string_when_endOfLine_is_in_the_middle(){
+        ReaderTest readerTest = new ReaderTest("co\nrona");
+        WriterTest writerTest = new WriterTest();
+        Copier copier = new Copier(readerTest,writerTest);
+        copier.copy();
+        assertEquals("co", writerTest.getWriteResult());
+    }
 
 
 }
@@ -55,6 +79,11 @@ class WriterTest implements Writeable {
 
     public int writeResultLength(){
         return writeResult.length();
+    }
+
+    public String getWriteResult(){
+
+        return writeResult;
     }
 
 
