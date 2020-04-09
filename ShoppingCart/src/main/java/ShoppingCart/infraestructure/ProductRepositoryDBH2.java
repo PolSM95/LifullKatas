44a52ba0b4ad3282d6269a0494d6f72c221ee6e5
Natalia Terrones <nataliaterrones@lifullconnect.com>
2020-04-09 09:25:11 +0200

    Feat: Modify product repository for a select

--- ShoppingCart/src/main/java/ShoppingCart/infraestructure/ProductRepositoryDBH2.java
+++ ShoppingCart/src/main/java/ShoppingCart/infraestructure/ProductRepositoryDBH2.java
@@ -3,28 +3,27 @@ package ShoppingCart.infraestructure;
 import ShoppingCart.domain.Product;
 import ShoppingCart.domain.ProductID;
 import ShoppingCart.domain.ProductRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.*;
 
 public class ProductRepositoryDBH2 implements ProductRespository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public Product getProductById(ProductID productID) {
-        try{
+        try {
 
-            Connection connection = DriverManager.getConnection("jdbc:h2:~/data", "sa", "");
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM PRODUCT WHERE idProduct = ?");
-            statement.setString(1, productID.toString());
-
-            ResultSet resultSet = statement.executeQuery();
-
-            Product product = new Product(new ProductID(resultSet.getInt("idProduct")),
-                    resultSet.getString("product_name"),
-                    resultSet.getDouble("product_value"));
-
-            connection.close();
+            Product product = jdbcTemplate.queryForObject("SELECT * FROM PRODUCT WHERE idProduct = ?",
+                    new Object[]{productID.toString()},
+                    (resultSet, rowNumber) -> new Product(new ProductID(
+                            resultSet.getInt("idProduct")),
+                            resultSet.getString("product_name"),
+                            resultSet.getDouble("product_value")));
             return product;
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
